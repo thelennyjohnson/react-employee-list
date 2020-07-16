7a49bebe19c3457060ec6109cc349a2178728e55
thelennyjohnson <lennyjohnson291@gmail.com>
2020-07-16 09:43:09 +0100

    Updates

---
+++ precache-manifest.471561d16bede50b99e7eb7c344ecef8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "159fb95a5722ff51f5750701fd3d8277",
+    "url": "/employee-list/index.html"
+  },
+  {
+    "revision": "a126ef1cc65bf3d4b3a6",
+    "url": "/employee-list/static/css/main.996b89ae.chunk.css"
+  },
+  {
+    "revision": "dc4734770c200758d612",
+    "url": "/employee-list/static/js/2.1adbc980.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/employee-list/static/js/2.1adbc980.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a126ef1cc65bf3d4b3a6",
+    "url": "/employee-list/static/js/main.e82e7b43.chunk.js"
+  },
+  {
+    "revision": "3d406899f5b278516c83",
+    "url": "/employee-list/static/js/runtime-main.432e7594.js"
+  }
+]);
